87cdfc9badaf341eb4cc15550c07124fff24e28b
hzchenxiaobin <87942859+hzchenxiaobin@users.noreply.github.com>
2021-08-14 23:42:36 +0800

    01背包

---
+++ ch7advance/zeroOneBag/Main.java
@@ -0,0 +1,21 @@
+package ch7advance.zeroOneBag;
+
+/**
+ * 01背包问题
+ */
+public class Main {
+    /**
+     * 常规的dp解法，用二维数组解决
+     * @param v 每个物品的体积
+     * @param w 每个物品的价值
+     * @param W 背包总体积
+     * @return 最大价值
+     */
+    public int solute(int[] v, int[] w, int W) {
+        int[][] dp = new int[v.length][W];
+        for(int i=0;i<v.length;i++) {
+            for(int j=W;j<)
+        }
+
+    }
+}
