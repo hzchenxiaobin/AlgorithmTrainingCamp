bf6b4a899ebd7a437edbc2d62b7a9c45f1973bf0
hzchenxiaobin <87942859+hzchenxiaobin@users.noreply.github.com>
2021-08-15 15:20:59 +0800

    完全背包问题

---
+++ ch7advance/completeBag/Main.java
@@ -0,0 +1,25 @@
+package ch7advance.completeBag;
+
+/**
+ * 完全背包问题
+ */
+public class Main {
+    /**
+     *
+     * @param v 价值
+     * @param w 体积
+     * @param W 总体积
+     * @return
+     */
+    private int solute(int[] v, int[] w, int W) {
+        int n = v.length + 1;
+        int[] dp = new int[W+1];
+        for(int i=1;i<=n;i++) {
+            for(int j=w[i];j<=W;j++) {
+                dp[j] = Math.max(dp[j], dp[j-w[i]] + v[i]);
+            }
+        }
+
+        return dp[W];
+    }
+}
